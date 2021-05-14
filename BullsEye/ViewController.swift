4ebdf5c6db38bd953c56345a062116f60ad43f99
yanjixian <315644588@qq.com>
2021-05-14 09:39:52 +0800

    lesssion 4

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,17 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var value = 50
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var targetLabel: UILabel!
+    
+    var value = 0
+    var targetValue = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+       startNewGame()
     }
     
    
     @IBAction func showAlert() {
         
-        let message = "The value of the slider is: \(value)"
+        let message = "The value of the slider is: \(value).\n" +
+        "The target value is: \(targetValue)"
 
         
           let alert = UIAlertController(
@@ -30,7 +38,7 @@ class ViewController: UIViewController {
           let action = UIAlertAction(
             title: "OK",
             style: .default,
-            handler: nil)
+            handler: {_ in self.startNewGame()})
 
           alert.addAction(action)
           present(alert, animated: true, completion: nil)
@@ -41,6 +49,17 @@ class ViewController: UIViewController {
         value = lroundf(slider.value)
     }
     
+    func startNewGame() {
+        targetValue = Int.random(in: 1...100)
+        value = 50
+        slider.value = Float(value)
+        updateLabels()
+        
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+    }
   
 }
 
